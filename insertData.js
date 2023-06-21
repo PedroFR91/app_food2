4944858ea3d7b075572101d3a0172ce8adc27bee
paolasp2 <pasanpe2001@gmail.com>
2023-06-21 17:34:38 +0200

    data

--- insertData.js
+++ insertData.js
@@ -1,32 +1,13 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase.config.js'); // Ruta al archivo JSON de las credenciales de Firebase
+// Ruta al archivo JSON con los datos
+const data = require('./data.json');
+const serviceAccount = require('./serviceAccount.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://prueba-34903-default-rtdb.europe-west1.firebasedatabase.app'
+  credential: admin.credential.cert(serviceAccount)
 });
 
-const firestore = admin.firestore();
+data.forEach(async (item) => {
+    await admin.firestore().collection('Recipes').add(item);
+  });
 
-// Ruta al archivo JSON con los datos
-const data = require('./functions/data.json');
-
-// Nombre de la colección en Firestore
-const collectionName = 'All Recipes';
-
-// Importar los datos a Firestore
-async function importData() {
-  const collectionRef = firestore.collection(collectionName);
-
-  // Recorrer los datos y agregarlos a la colección
-  for (const documentId in data) {
-    const documentData = data[documentId];
-    await collectionRef.doc(documentId).set(documentData);
-  }
-
-  console.log('Datos importados exitosamente.');
-}
-
-importData().catch(error => {
-  console.error('Error al importar los datos:', error);
-});
